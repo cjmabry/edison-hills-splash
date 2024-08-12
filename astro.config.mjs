473bcee0d49b19b53a5fea6f15af755ec9a0781f
cjmabry <chris@withtheranks.com>
2024-08-12 18:51:20 -0500

    Fix base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://cjmabry.github.io',
-    base: '/edison-hills-splash',
+    site: 'https://edisonhills.com',
+    base: '/',
 });
